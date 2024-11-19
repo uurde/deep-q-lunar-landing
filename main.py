e08e345f72885d4610b642492bedbce60db5146b
Uğur Değirmenci <m.ugurdegirmenci@gmail.com>
2024-11-19 13:54:23 +0300

    Implementing the DQN class

--- main.py
+++ main.py
@@ -64,3 +64,49 @@ class ReplayMemory(object):
         next_states = torch.from_numpy(np.vstach([e[3] for e in experiences if e is not None])).float().to(self.device)
         dones = torch.from_numpy(np.vstach([e[4] for e in experiences if e is not None])).astype(np.uint8).float().to(self.device)
         return states, next_states, actions, rewards, dones
+
+# Implementing the DQN class
+class Agent():
+    def __init__(self, state_size, action_size):
+        self.device = torch.device("cuda:0" if torch.cude.is_available() else "cpu")
+        self.state_size = state_size
+        self.action_size = action_size
+        self.local_qnetwork = Network(state_size, action_size).to(self.device)
+        self.target_qnetwork = Network(state_size, action_size).to(self.device)
+        self.optimizer = optim.Adam(self.local_qnetwork.parameters(), lr = learning_rate)
+        self.memory = ReplayMemory(replay_buffer_size)
+        self.t_step = 0
+    
+    def step(self, state, action, reward, next_state, done):
+        self.memory.push((state, action, reward, next_state, done))
+        self.t_step = (self.t_step + 1) % 4
+        if self.t_step == 0:
+            if len(self.memory.memory) > minibatch_size:
+                experiences = self.memory.sample(100)
+                self.learn(experiences, discount_factor)
+    
+    def act(self, state, epsilon = 0.):
+        state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
+        self.local_qnetwork.eval()
+        with torch.no_grad():
+            action_values = self.local_qnetwork(state)
+        self.local_qnetwork.train()
+        if random.random() > epsilon:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arrange(self.action_size))
+        
+    def learn(self, experiences, discount_factor):
+        states, next_states, actions, rewards, dones = experiences
+        next_q_targets = self.target_qnetwork(next_states).detach().max(1)[0].unsqueeze(1)
+        q_targets = rewards + (discount_factor * next_q_targets * (1 - dones))
+        q_expected = self.local_qnetwork(states).gather(1, actions)
+        loss = F.mse_loss(q_expected, q_targets)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        self.soft_update(self.local_qnetwork, self.target_qnetwork, interpolation_parameter)
+
+    def soft_update(self, local_model, target_model, interpolation_parameter):
+        for target_param, local_param in zip(target_model.parameters(), local_model.paramters()):
+            target_param.data.copy_(interpolation_parameter * local_param.data + (1.0 - interpolation_parameter) * target_param.data)
