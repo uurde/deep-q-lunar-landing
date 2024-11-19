958efd8de654c13de12f42e070d6915141dcec8e
Uğur Değirmenci <m.ugurdegirmenci@gmail.com>
2024-11-19 15:41:33 +0300

    Training the DQN agent

--- main.py
+++ main.py
@@ -58,11 +58,11 @@ class ReplayMemory(object):
     
     def sample(self, batch_size):
         experiences = random.sample(self.memory, k = batch_size)
-        states = torch.from_numpy(np.vstach([e[0] for e in experiences if e is not None])).float().to(self.device)
-        actions = torch.from_numpy(np.vstach([e[1] for e in experiences if e is not None])).long().to(self.device)
-        rewards = torch.from_numpy(np.vstach([e[2] for e in experiences if e is not None])).float().to(self.device)
-        next_states = torch.from_numpy(np.vstach([e[3] for e in experiences if e is not None])).float().to(self.device)
-        dones = torch.from_numpy(np.vstach([e[4] for e in experiences if e is not None])).astype(np.uint8).float().to(self.device)
+        states = torch.from_numpy(np.vstack([e[0] for e in experiences if e is not None])).float().to(self.device)
+        actions = torch.from_numpy(np.vstack([e[1] for e in experiences if e is not None])).long().to(self.device)
+        rewards = torch.from_numpy(np.vstack([e[2] for e in experiences if e is not None])).float().to(self.device)
+        next_states = torch.from_numpy(np.vstack([e[3] for e in experiences if e is not None])).float().to(self.device)
+        dones = torch.from_numpy(np.vstack([e[4] for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
         return states, next_states, actions, rewards, dones
 
 # Implementing the DQN class
@@ -94,7 +94,7 @@ class Agent():
         if random.random() > epsilon:
             return np.argmax(action_values.cpu().data.numpy())
         else:
-            return random.choice(np.arrange(self.action_size))
+            return random.choice(np.arange(self.action_size))
         
     def learn(self, experiences, discount_factor):
         states, next_states, actions, rewards, dones = experiences
@@ -108,8 +108,38 @@ class Agent():
         self.soft_update(self.local_qnetwork, self.target_qnetwork, interpolation_parameter)
 
     def soft_update(self, local_model, target_model, interpolation_parameter):
-        for target_param, local_param in zip(target_model.parameters(), local_model.paramters()):
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(interpolation_parameter * local_param.data + (1.0 - interpolation_parameter) * target_param.data)
 
 # Initializing the DQN agent
 agent = Agent(state_size, number_actions)
+
+# Training the DQN agent
+number_episodes = 2000
+maximum_number_timesteps_per_episode = 1000
+epsilon_starting_value = 1.0
+epsilon_ending_value = 0.01
+epsilon_decay_value = 0.995
+epsilon = epsilon_starting_value
+scores_on_100_episodes = deque(maxlen = 100)
+
+for episode in range(1, number_episodes + 1):
+    state, _ = env.reset()
+    score = 0
+    for t in range(maximum_number_timesteps_per_episode):
+        action = agent.act(state, epsilon)
+        next_state, reward, done, _, _ = env.step(action)
+        agent.step(state, action, reward, next_state, done)
+        state = next_state
+        score += reward
+        if done:
+            break
+    scores_on_100_episodes.append(score)
+    epsilon = max(epsilon_ending_value, epsilon_decay_value * epsilon)
+    print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode, np.mean(scores_on_100_episodes)), end = "")
+    if episode % 100 == 0:
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(episode, np.mean(scores_on_100_episodes)))
+    if np.mean(scores_on_100_episodes) >= 200.0:
+        print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(episode - 100, np.mean(scores_on_100_episodes)))
+        torch.save(agent.local_qnetwork.state_dict(), 'checkpoint.pth')
+        break
