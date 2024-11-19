d551039a7c6778c4736f5649f8f7dada8800b276
Uğur Değirmenci <m.ugurdegirmenci@gmail.com>
2024-11-19 10:58:49 +0300

    Creating the architecture of the Neural Network

---
+++ main.py
@@ -0,0 +1,45 @@
+# Importing the libraries
+import os
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.autograd as autograd
+from torch.autograd import Variable
+from collections import deque, namedtuple
+
+# Creating the architecture of the Neural Network
+class Network(nn.Module):
+
+    def __init__(self, state_size, action_size, seed = 42):
+        super(Network, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, action_size)
+
+    def forward(self, state):
+        x = self.fc1(state)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        return self.fc3(x)
+
+    # Setting up the environment
+    import gymnasium as gym
+    env = gym.make('LunarLander-v3')
+    state_shape = env.observation_space.shape
+    state_size = env.observation_space.shape[0]
+    number_actions = env.action_space.n
+    print('State shape: ', state_shape)
+    print('State size: ', state_size)
+    print('Number Actions: ', number_actions)
+
+    # Initializing the hyperparameters
+    learning_rate = 5e-4 #0.0005
+    minibatch_size = 100
+    discount_factor = 0.99
+    replay_buffer_size = int(1e5) #100000
+    interpolation_parameter = 1e-3
